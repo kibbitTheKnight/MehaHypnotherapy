c9918473660bf07e51a652bec3e7db1ec469e839
Keira <kibbitintheknight@knights.ucf.edu>
2018-10-21 16:26:58 -0400

    added bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-//const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const path = require('path');
 const bodyparser = require('body-parser');
 const express = require('express');
