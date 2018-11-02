25c6ab63c7b6b51262574cfe8f568eebcb78f244
Keira <kibbitintheknight@knights.ucf.edu>
2018-11-02 18:35:31 -0400

    Reverting

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.get('/login', function (req, res) {
 
 app.post('/login', function (req, response) 
 {
-	var loginLink = $("#loginbutton");
+	//var loginLink = $("#loginbutton");
 	console.log("receiving login info:");
 	if (req.body.username > 30 ||
 		checkInput(req.body)) 
@@ -181,7 +181,7 @@ app.post('/login', function (req, response)
 								console.log("redirecting to home");
 								response.set('Access-Control-Allow-Origin','*');
 								response.status(200).send();
-								loginLink.innerHTML = "<a href=\"/login\" id = \"loginbutton\">Log Out</a>";
+								//loginLink.innerHTML = "<a href=\"/login\" id = \"loginbutton\">Log Out</a>";
 								return;
 							} 
 							else 
