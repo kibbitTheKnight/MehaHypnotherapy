dccf2c5e5ba371f360ce99c5b4bbc68eaae2fdd4
Keira <kibbitintheknight@knights.ucf.edu>
2018-11-07 22:11:14 -0500

    Moved log in stuff

--- js/func.js
+++ js/func.js
@@ -92,8 +92,27 @@ $(document).ready(function() {
 		
 		footer.html(content);
 	})
-	
-	//determine whether logged in or out
+})
+
+
+let userInfo =
+{
+	username: "",
+	loggedin: false
+}
+
+function logout()
+{
+	$.post("/logout", function (res, status) {
+	  window.location.replace('/login');
+     }).fail(function () {
+          alert("Something went wrong??");
+     })
+}
+
+
+window.addEventListener("load", function load(event){
+		//determine whether logged in or out
 	$.post('/getstate', function(res, status)
 	{
 		//get username and logged in status
@@ -123,26 +142,4 @@ $(document).ready(function() {
 	{
 		alert("Something went wrong??");
 	});
-})
-
-
-let userInfo =
-{
-	username: "",
-	loggedin: false
-}
-
-function logout()
-{
-	$.post("/logout", function (res, status) {
-	  window.location.replace('/login');
-     }).fail(function () {
-          alert("Something went wrong??");
-     })
-}
-
-
-window.addEventListener("load", function load(event){
-	
-	
 },false);
